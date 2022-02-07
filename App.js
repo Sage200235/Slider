12fb2584eb8d00f59fe97f2abe6c9b18cf8dfb75
Sage200235 <mueezolatunde@gmail.com>
2022-02-07 17:03:53 +0100

    new

---
+++ App.js
@@ -0,0 +1,33 @@
+import Header from './Components/header'
+import Waterson from './Components/waterson'
+import Banner from './Components/Banner'
+import Friction from './Components/friction'
+import Posthero from './Components/posthero'
+import Footer from './Components/footer'
+import Prefooter from './Components/prefooter'
+import Hero from './Components/Hero'
+import Aspire from './Components/aspire'
+import Head from './Components/head'
+import Slider from './Components/Slider'
+import 'antd/dist/antd.css';
+
+
+
+function App(){
+  return (
+    <div>
+    {/* <Head/> */}
+    <Slider />
+    {/* <Hero/>
+    <Posthero/>
+    <Friction/>
+    <Waterson/>
+    <Aspire/>
+    <Banner/>
+    <Prefooter/>
+    <Footer/>   */}
+    </div>
+  );
+}
+
+export default App;
